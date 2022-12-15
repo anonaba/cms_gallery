1dfb954327ffd6ebc83d9a68049fc288f23224d8
anonaba <anonabason@gmail.com>
2022-12-15 18:08:29 +0800

    added panel into the admin dashboard

--- admin/includes/admin_content.php
+++ admin/includes/admin_content.php
@@ -7,95 +7,107 @@
                             Blank Page
                             <small>Subheading</small>
                         </h1>
-                        <?php 
-                        // $user_found = User::find_user_by_id(1);
-                        // echo $user_found->id;
-                        //     echo '<pre>';
-                        //  var_dump($user_found);
-                        // echo '</pre>';
- 
-                        // $user = User::find_user_by_id(10);
-                        // $user->username = "spidy";
-                        // $user->save();
-                        // $photo = Photo::find_by_id(8);
-                        // echo $photo;
+                        <div class="row">
+                            <div class="col-lg-3 col-md-6">
+                               <div class="panel panel-primary">
+                                    <div class="panel-heading">
+                                        <div class="row">
+                                            <div class="col-xs-3">
+                                                <i class="fa fa-users fa-5x"></i>
+                                            </div>
+                                            <div class="col-xs-9 text-right">
+                                                <div class="huge">4</div>
+                                                <div>New Views</div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <a href="#">
+                                        <div class="panel-footer">              
+                                            <span class="pull-left">View Details</span>
+                                            <span class="pull-right">
+                                                <i class="fa fa-arrow-circle-right"></i>
 
-                        // if(isset($user->id)) {
-                        //     $user->delete();
-                        //     echo 'Deleted';
-                        // } else {
-                        //     echo "No such users";
-                        // }
-                            // $user = User::find_user_by_id(19);
+                                            </span>
+                                            <div class="clearfix"></div>
+                                        </div>
+                                    </a>
+                               </div>                                
+                            </div>
+                                 <div class="col-lg-3 col-md-6">
+                               <div class="panel panel-success">
+                                    <div class="panel-heading">
+                                        <div class="row">
+                                            <div class="col-xs-3">
+                                                <i class="fa fa-photo fa-5x"></i>
+                                            </div>
+                                            <div class="col-xs-9 text-right">
+                                                <div class="huge">4</div>
+                                                <div>Photos</div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <a href="#">
+                                        <div class="panel-footer">
+                                            <span class="pull-left">Total Photos In Gallery</span>
+                                            <span class="pull-right">
+                                                <i class="fa fa-arrow-circle-right"></i>
 
-                            // $user->username = "jay";
-                            // $user->save();
+                                            </span>
+                                            <div class="clearfix"></div>
+                                        </div>
+                                    </a>
+                               </div>                                
+                            </div>
+                                 <div class="col-lg-3 col-md-6">
+                               <div class="panel panel-warning">
+                                    <div class="panel-heading">
+                                        <div class="row">
+                                            <div class="col-xs-3">
+                                                <i class="fa fa-user fa-5x"></i>
+                                            </div>
+                                            <div class="col-xs-9 text-right">
+                                                <div class="huge">4</div>
+                                                <div>Users</div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <a href="#">
+                                        <div class="panel-footer">
+                                            <span class="pull-left">Total Users</span>
+                                            <span class="pull-right">
+                                                <i class="fa fa-arrow-circle-right"></i>
 
-                        // $users = User::find_all();
-                        // foreach($users as $user ) {
-                        //     echo $user->username.'<br>';
-                        // }
-
-                        // $user = User::find_by_id(2);
-                        // echo $user->username.'<br>';
-                        // foreach($users as $user) {
-                        //     echo $user->username.'<br>';
-                        // }
-
-                        // $comma_separated =implode("`,`",array_keys($user->properties()));
-                        // $comma_separated = "`".$comma_separated."`";
-                        // echo $comma_separated;
-                        // $user = User::find_user_by_id(20);
-                        // $user = new User;
-                        // $user->username = "eve";
-                        // $user->password = "5657547";
-                        // $user->first_name = "Eve";
-                        // $user->last_name = "Day";
-                        // echo $user->save();
-
-                        //   $photos = Photo::find_all();
-                        //     foreach($photos as $photo ) {
-                        //     echo $photo->filename.'<br>';
-                        // }
-                        // $photo->description = "Donec tincidunt augue et ullamcorper mattis. Fusce nec mauris ut urna rhoncus tristique. Nam vel leo pulvinar, venenatis sapien at, eleifend nisi.";
-                        // $photo->filename = "dove.jpg";
-                        // $photo->type = "image";
-                        // $photo->size = "446464";
-                        // echo $photo->save();
-
-                        // $photo = new Photo;
-
-                        // $photo->title = "Car 1";
-                        //   $photo->description = "Donec tincidunt augue et ullamcorper mattis. Fusce nec mauris ut urna rhoncus tristique. Nam vel leo pulvinar, venenatis sapien at, eleifend nisi.";
-                        // $photo->filename = "dove.jpg";
-                        // $photo->type = "image";
-                        // $photo->size = "446464";
-                        // if($photo->save()) {
-                        // // echo '<pre>';
-                        // //  var_dump($photo);
-                        // // echo '</pre>';
-                        //     echo $photo->save();
-
-                        // } else {
-                        //      $message = join("<br>", $photo->errors);
-                        //      echo $message;
-                        //      echo 'no';
-                        // }
-
-
-
-
-                    
-
-   ?>
-                        <ol class="breadcrumb">
-                            <li>
-                                <i class="fa fa-dashboard"></i>  <a href="index.html">Dashboard</a>
-                            </li>
-                            <li class="active">
-                                <i class="fa fa-file"></i> Blank Page
-                            </li>
-                        </ol>
+                                            </span>
+                                            <div class="clearfix"></div>
+                                        </div>
+                                    </a>
+                               </div>                                
+                            </div>
+                                 <div class="col-lg-3 col-md-6">
+                               <div class="panel panel-danger">
+                                    <div class="panel-heading">
+                                        <div class="row">
+                                            <div class="col-xs-3">
+                                                <i class="fa fa-wheel fa-5x"></i>
+                                            </div>
+                                            <div class="col-xs-9 text-right">
+                                                <div class="huge">4</div>
+                                                <div>Comments</div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <a href="#">
+                                        <div class="panel-footer">
+                                            <span class="pull-left">Total Comments</span>
+                                            <span class="pull-right">
+                                                <i class="fa fa-arrow-circle-right"></i>
+                                            </span>
+                                            <div class="clearfix"></div>
+                                        </div>
+                                    </a>
+                               </div>                                
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <!-- /.row -->
