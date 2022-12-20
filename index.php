11d20cb37d8801c4247a6205f0beda710486ea4c
anonaba <anonabason@gmail.com>
2022-12-20 15:44:07 +0800

    added gallery system functions

--- index.php
+++ index.php
@@ -1,25 +1,34 @@
 <?php include("includes/header.php"); ?>
-
+<?php
+    $photos = Photo::find_all();
+?>
 
         <div class="row">
 
             <!-- Blog Entries Column -->
-            <div class="col-md-8">
+            <div class="col-md-12">
+                <div class="thumbnail row">
+                    
+                
+                    <?php foreach($photos as $photo) : ?>
+                        <div class="col-xs-6 col-md-3">
+                            <a class="thumbnail" href="photo.php?id=<?= $photo->id; ?>">
+                                <img src="admin/<?= $photo->picture_path(); ?>" alt="">
+                            </a>
+                        </div>
+                    <?php endforeach; ?>
 
-    
-            
-          
-         
+                </div>
 
             </div>
             <!-- Blog Sidebar Widgets Column -->
-            <div class="col-md-4">
+            <!-- <div class="col-md-4"> -->
 
             
-                 <?php include("includes/sidebar.php"); ?>
+                 <?php //include("includes/sidebar.php"); ?>
 
 
 
-            </div>
+            <!-- </div> -->
         <!-- /.row -->
         <?php include("includes/footer.php"); ?>
